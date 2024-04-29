d5ee37b1b46f9a764546d8c77bbf4fdcb74dc42b
anar <anarb2536@gmail.com>
2024-04-29 20:00:24 +0400

    init

--- src/ders1.java
+++ src/ders1.java
@@ -1,5 +1,10 @@
 public class ders1 {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        for (int first = 1;first < 10;first++) {
+            for (int second = 1;second < 10;second++) {
+                System.out.println(first + " * " + second + " = " + first * second);
+            }
+            System.out.println("---------------------");
+        }
     }
 }
