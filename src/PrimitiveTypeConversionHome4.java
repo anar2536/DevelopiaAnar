343f36bf4d22c09ac5cabdd310b45a455c1cf876
anar <anarb2536@gmail.com>
2024-05-02 12:03:52 +0400

    init

---
+++ src/PrimitiveTypeConversionHome4.java
@@ -0,0 +1,7 @@
+public class PrimitiveTypeConversionHome4 {
+    public static void main(String[] args) {
+        long x = 92523635483L;
+        int y = (int)x;
+        System.out.println(y);
+    }
+}
