60e2403d193bf01579fad8833de6f55f08a8274d
anar <anarb2536@gmail.com>
2024-05-02 12:05:33 +0400

    init

---
+++ src/PrimitiveTypeConversionHome5.java
@@ -0,0 +1,7 @@
+public class PrimitiveTypeConversionHome5 {
+    public static void main(String[] args) {
+        double x = 65787.3;
+        int y = (int)x;
+        System.out.println(y);
+    }
+}
