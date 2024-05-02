016da1abd09275fb515e744aa144e10691990ae3
anar <anarb2536@gmail.com>
2024-05-02 11:58:45 +0400

    init

---
+++ src/PrimitiveTypeConversionHome2.java
@@ -0,0 +1,7 @@
+public class PrimitiveTypeConversionHome2 {
+    public static void main(String[] args) {
+        short x = 636;
+        byte y = (byte)x;
+        System.out.println(y);
+    }
+}
