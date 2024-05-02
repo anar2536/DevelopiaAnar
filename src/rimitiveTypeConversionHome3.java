45a0cd38e2289ccf4a07a04bf956e5582002c473
anar <anarb2536@gmail.com>
2024-05-02 12:00:54 +0400

    init

---
+++ src/rimitiveTypeConversionHome3.java
@@ -0,0 +1,7 @@
+public class rimitiveTypeConversionHome3 {
+    public static void main(String[] args) {
+        long x = 458;
+        short y = (short)x;
+        System.out.println(y);
+    }
+}
