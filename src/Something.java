b18db0b2ba4a12e81b59b6c3138169742573f95a
anar <anarb2536@gmail.com>
2024-06-06 15:56:37 +0400

    init

--- src/Something.java
+++ src/Something.java
@@ -1,12 +1,55 @@
-import Animal.*;
+class Person {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String phone;
+    private String address;
+    public Person(int id, String name, String surname, int age, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.phone = phone;
+        this.address = address;
+    }
+    public void printInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+    }
+}
+
+class Employee extends Person {
+    private double salary;
+    private String department;
+    private String username;
+    private String password;
+    public Employee(int id, String name, String surname, int age, String phone, String address,
+                    double salary, String department, String username, String password) {
+        super(id, name, surname, age, phone, address);
+        this.salary = salary;
+        this.department = department;
+        this.username = username;
+        this.password = password;
+    }
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Salary: " + salary);
+        System.out.println("Department: " + department);
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+}
+
 public class Something {
     public static void main(String[] args) {
-        Cat cat = new Cat("Whiskers", 3);
-        Dog dog = new Dog("Buddy", 5);
-        Lion lion = new Lion("Simba", 7);
-
-        cat.makeNoise();
-        dog.makeNoise();
-        lion.makeNoise();
+        Employee employee = new Employee(1, "John", "Doe", 30, "123456789", "123 Main St",
+                50000.0, "IT", "johndoe", "password123");
+        employee.printInfo();
     }
 }
