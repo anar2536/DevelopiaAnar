f7c0802e6ffe40906cd19031643e99c2ece96da0
anar <anarb2536@gmail.com>
2024-09-21 12:57:57 +0200

    init

---
+++ src/anar/src/main/java/az/bizimkler/anar/AnarApplication.java
@@ -0,0 +1,13 @@
+package az.bizimkler.anar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarApplication.class, args);
+	}
+
+}
---
+++ src/anar/src/test/java/az/bizimkler/anar/AnarApplicationTests.java
@@ -0,0 +1,13 @@
+package az.bizimkler.anar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
