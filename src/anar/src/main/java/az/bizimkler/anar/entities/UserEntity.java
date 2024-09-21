107bb29509b948e4e62b50d2ba80709aa2fcf663
anar <anarb2536@gmail.com>
2024-09-21 13:05:26 +0200

    init

---
+++ src/anar/src/main/java/az/bizimkler/anar/entities/UserEntity.java
@@ -0,0 +1,26 @@
+package anar.src.main.java.az.bizimkler.anar.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private Integer enabled;
+
+}
