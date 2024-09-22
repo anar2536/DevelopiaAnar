ea916b4797ef14579a826fef8e9bfff19f45bc3e
anar <anarb2536@gmail.com>
2024-09-22 12:57:27 +0200

    init

--- src/anar/src/main/java/az/bizimkler/anar/service/StudentService.java
+++ src/anar/src/main/java/az/bizimkler/anar/service/StudentService.java
@@ -1,23 +1,29 @@
 package anar.src.main.java.az.bizimkler.anar.service;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import anar.src.main.java.az.bizimkler.anar.repository.StudentRepository;
+import java.util.List;
 import anar.src.main.java.az.bizimkler.anar.entities.StudentEntity;
-
-import lombok.RequiredArgsConstructor;
+import anar.src.main.java.az.bizimkler.anar.repository.StudentRepository;
 
 @Service
 public class StudentService {
+
     @Autowired
     private StudentRepository studentRepository;
 
+    // Öğrenci kaydetme
     public StudentEntity saveStudent(StudentEntity student) {
         return studentRepository.save(student);
     }
 
+    // Tüm öğrencileri listeleme
     public List<StudentEntity> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    // ID'ye göre öğrenci bulma
+    public StudentEntity getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
     }
 }
