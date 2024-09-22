125d443246226cb48dc18cd02441c26879cffcc3
anar <anarb2536@gmail.com>
2024-09-22 12:30:28 +0200

    init

--- src/anar/src/main/java/az/bizimkler/anar/repository/StudentRepository.java
+++ src/anar/src/main/java/az/bizimkler/anar/repository/StudentRepository.java
@@ -1,9 +1,8 @@
 package anar.src.main.java.az.bizimkler.anar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import anar.src.main.java.az.bizimkler.anar.entities.StudentEntity;
 
-public interface OrderRepository extends JpaRepository<StudentEntity, Long> {
+public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
 
 }
