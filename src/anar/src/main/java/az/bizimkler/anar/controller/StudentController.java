ae16d60dacd95be8944815ed71f2634ea85b5314
anar <anarb2536@gmail.com>
2024-09-22 13:02:47 +0200

    init

--- src/anar/src/main/java/az/bizimkler/anar/controller/StudentController.java
+++ src/anar/src/main/java/az/bizimkler/anar/controller/StudentController.java
@@ -1,14 +1,14 @@
 package anar.src.main.java.az.bizimkler.anar.controller;
 
-package anar.src.main.java.az.bizimkler.anar.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import anar.src.main.java.az.bizimkler.anar.entities.StudentEntity;
+import anar.src.main.java.az.bizimkler.anar.entities.BookEntity;
 import anar.src.main.java.az.bizimkler.anar.service.StudentService;
+import anar.src.main.java.az.bizimkler.anar.service.BookService;
 
 import java.util.List;
 
@@ -18,6 +18,9 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @Autowired
+    private BookService bookService;
 
     @PostMapping("/save")
     public ResponseEntity<StudentEntity> saveStudent(@RequestBody StudentEntity student) {
@@ -29,6 +32,7 @@ public class StudentController {
     public ResponseEntity<List<StudentEntity>> getAllStudents() {
         List<StudentEntity> students = studentService.getAllStudents();
         return new ResponseEntity<>(students, HttpStatus.OK);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<StudentEntity> getStudentById(@PathVariable("id") Long id) {
@@ -39,5 +43,18 @@ public class StudentController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-}
 
+    @PostMapping("/{studentId}/books/{bookId}")
+    public ResponseEntity<String> assignBookToStudent(@PathVariable Long studentId, @PathVariable Long bookId) {
+        StudentEntity student = studentService.getStudentById(studentId);
+        BookEntity book = bookService.getBookById(bookId);
+
+        if (student != null && book != null) {
+            student.getBooks().add(book);
+            studentService.saveStudent(student);
+            return new ResponseEntity<>("Kitap öğrenciye verildi.", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Öğrenci veya kitap bulunamadı.", HttpStatus.NOT_FOUND);  Found
+        }
+    }
+}
