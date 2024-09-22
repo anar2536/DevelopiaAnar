425aec6b2e52d6780f5ca18da4d2053fdea70003
anar <anarb2536@gmail.com>
2024-09-22 11:52:18 +0200

    init

--- src/anar/src/main/java/az/bizimkler/anar/entities/BookEntity.java
+++ src/anar/src/main/java/az/bizimkler/anar/entities/BookEntity.java
@@ -2,10 +2,12 @@ package anar.src.main.java.az.bizimkler.anar.entities;
 
 import java.time.LocalDateTime;
 
+import java.util.Set;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,7 +20,6 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class BookEntity {
 
     @Id
@@ -37,4 +38,6 @@ public class BookEntity {
 
     private LocalDateTime registerDate;
 
+    @ManyToMany(mappedBy = "books")
+    private Set<StudentEntity> students;
 }
