fba5af35d78acfdf3fc5042f6cab3b5a7a075921
anar <anarb2536@gmail.com>
2024-09-22 12:47:53 +0200

    init

--- src/anar/src/main/java/az/bizimkler/anar/service/BookService.java
+++ src/anar/src/main/java/az/bizimkler/anar/service/BookService.java
@@ -12,11 +12,33 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    // Kitap kaydetme
     public BookEntity saveBook(BookEntity book) {
-        return BookRepository.save(book);
+        return bookRepository.save(book);
     }
 
+    // Tüm kitapları listeleme
     public List<BookEntity> getAllBooks() {
-        return BookRepository.findAll();
+        return bookRepository.findAll();
+    }
+
+    // Sayfalama ile kitap listeleme
+    public List<BookEntity> getBooksByPagination(Integer begin, Integer length) {
+        return bookRepository.findAllPagination(begin, length);
+    }
+
+    // Toplam kitap sayısını getirme
+    public Integer getTotalBooksCount() {
+        return bookRepository.findAllCount();
+    }
+
+    // İsme göre kitap arama
+    public List<BookEntity> searchBooksByName(String name) {
+        return bookRepository.findAllSearch(name);
+    }
+
+    // Creator'a göre sayfalama ile kitap listeleme
+    public List<BookEntity> getBooksByCreatorPagination(String creator, Integer begin, Integer length) {
+        return bookRepository.findAllByCreatorPagination(creator, begin, length);
     }
 }
